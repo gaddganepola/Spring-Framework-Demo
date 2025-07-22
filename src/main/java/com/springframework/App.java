3be5f7008324814b27a97c65f71e2a472a1e1d54
gaddganepola <ganeypola@gmail.com>
2025-07-22 08:55:36 +0530

    spring bean xml configuration

--- src/main/java/com/springframework/App.java
+++ src/main/java/com/springframework/App.java
@@ -3,19 +3,16 @@ package com.springframework;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Alien obj;
 //      Manually created object
 //        obj = new Alien();
 
 //        Use application context to create object
-        ApplicationContext context = new ClassPathXmlApplicationContext();
-        obj = (Alien)context.getBean("alien");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        obj = (Alien) context.getBean("alien");
 
         obj.coding();
-
     }
 }
