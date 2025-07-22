1e4ac30199852d7490f277355518149845da669a
gaddganepola <ganeypola@gmail.com>
2025-07-22 19:35:55 +0530

    constructor injection

--- src/main/java/com/springframework/Alien.java
+++ src/main/java/com/springframework/Alien.java
@@ -1,12 +1,42 @@
 package com.springframework;
 
+import java.beans.ConstructorProperties;
+
 public class Alien {
 
     private int age;
-    private Laptop lap = new Laptop();
+    private int salary;
+    private Laptop lap;
 
     public Alien() {
         System.out.println("Alien Created..");
+    }
+
+    public Alien(int age) {
+        this.age = age;
+        System.out.println("age constructor called...");
+    }
+
+    public Alien(int age, Laptop lap) {
+        this.age = age;
+        this.lap = lap;
+        System.out.println("age and lap constructor called...");
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @ConstructorProperties({"age", "salary", "lap"})
+    public Alien(int age, int salary, Laptop lap) {
+        this.age = age;
+        this.salary = salary;
+        this.lap = lap;
+        System.out.println("All constructor called...");
     }
 
     public void coding() {
--- src/main/java/com/springframework/App.java
+++ src/main/java/com/springframework/App.java
@@ -13,17 +13,18 @@ public class App {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         obj = (Alien) context.getBean("alien");
 //        obj.setAge(10);
-        System.out.println(obj.getAge());
+        System.out.println("Age" + obj.getAge());
+        System.out.println("Salary" + obj.getSalary());
         obj.coding();
 
-        Alien obj1 = (Alien) context.getBean("alien");
-        System.out.println(obj1.getAge());
-        obj1.coding();
-
-        Laptop objlap1 = (Laptop) context.getBean(Laptop.class);
-        objlap1.compile();
-
-        Laptop objlap2 = (Laptop) context.getBean(Laptop.class);
-        objlap2.compile();
+//        Alien obj1 = (Alien) context.getBean("alien");
+//        System.out.println(obj1.getAge());
+//        obj1.coding();
+//
+//        Laptop objlap1 = (Laptop) context.getBean(Laptop.class);
+//        objlap1.compile();
+//
+//        Laptop objlap2 = (Laptop) context.getBean(Laptop.class);
+//        objlap2.compile();
     }
 }
